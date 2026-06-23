98e3eca7da6fb303edbd43a39afed4ce21f951b2
SuchitaGanati <suchitaganati2002@gmail.com>
2026-06-24 01:00:06 +0530

    Frontend setup

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div>
+      page 
+      <Button variant="brown" size="sm" text="click mee"/>
+    </div>
+  );
+}

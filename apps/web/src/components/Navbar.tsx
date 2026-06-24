ca0cfca887775e109fe0305695f5e071b9cbfddb
SuchitaGanati <suchitaganati2002@gmail.com>
2026-06-24 12:01:07 +0530

    header done

---
+++ apps/web/src/components/Navbar.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { nomenclature } from "@/constants/nomenclature";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <div className="h-16 px-6 flex items-center justify-between bg-[#765847]/10">
+      <div className="flex items-center gap-3">
+        <Link href="/">
+          <p className="text-xl font-extrabold text-[#231005]">
+            {nomenclature.PRODUCT_NAME}
+          </p>
+        </Link>
+      </div>
+      <div className="hidden md:flex items-center gap-6">
+        <Link
+          href="/"
+          className={`text-sm hover:underline underline-offset-8 ${
+            pathname === "/"
+              ? "font-bold underline decoration-amber-500 decoration-2"
+              : "font-light"
+          }`}
+        >
+          {nomenclature.HOME}
+        </Link>
+        <Link
+          href="/menu"
+          className={`text-sm hover:underline underline-offset-8 ${
+            pathname === "/Menu"
+              ? "font-bold underline decoration-amber-500 decoration-2"
+              : "font-light"
+          }`}
+        >
+          {nomenclature.MENU}
+        </Link>
+        <Link
+          href="/gallery"
+          className={`text-sm hover:underline underline-offset-8 ${
+            pathname === "/gallery"
+              ? "font-bold underline decoration-amber-500 decoration-2"
+              : "font-light"
+          }`}
+        >
+          {nomenclature.GALLERY}
+        </Link>
+        <Link
+          href="/about"
+          className={`text-sm hover:underline underline-offset-8 ${
+            pathname === "/about"
+              ? "font-bold underline decoration-amber-500 decoration-2"
+              : "font-light"
+          }`}
+        >
+          {nomenclature.ABOUT}
+        </Link>
+        <Link
+          href="/contact"
+          className={`text-sm hover:underline underline-offset-8 ${
+            pathname === "/contact"
+              ? "font-bold underline decoration-amber-500 decoration-2"
+              : "font-light"
+          }`}
+        >
+          {nomenclature.CONTACT}
+        </Link>
+      </div>
+      <div>
+        <Button variant="brown" text="Call to Order" size="sm" />
+      </div>
+    </div>
+  );
+};
+export default Navbar;
--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 px-3",
+        sm: "h-9 px-5",
         lg: "h-11 px-8",
         short: "h-7 px-3 text-xs",
       },

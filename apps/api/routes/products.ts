116a76c1a74b50d866d5c0b68aeb3cb798d60427
Pranita R <pranita1852@gmail.com>
2026-06-27 12:46:50 +0530

    Added: Category and product management with cloudinary

---
+++ apps/api/routes/products.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+import { authMiddleware } from '../middleware/auth'
+import { validate } from '../middleware/validations'
+import { ProductSchema, UpdateProductSchema } from '../../../packages/shared/schema/index'
+import {
+  getProducts,
+  getFeaturedProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/products'
+import { deleteProductImage, uploadProductImage } from '../controllers/cloundinary'
+import { upload } from '../middleware/cloundinary'
+
+const router = Router()
+
+// cloudinary
+router.post('/image/upload', authMiddleware, upload.single('image'), uploadProductImage)
+router.delete('/image/delete', authMiddleware, deleteProductImage)
+
+router.post('/create', authMiddleware, validate(ProductSchema), createProduct)
+router.post('/update/:id', authMiddleware, validate(UpdateProductSchema), updateProduct)
+router.post('/delete/:id', authMiddleware, deleteProduct)
+
+router.get('/', getProducts)
+router.get('/featured', getFeaturedProducts)
+
+export default router

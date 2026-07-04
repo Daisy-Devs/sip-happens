934da732e021ac20c5a9f7fc2c3b11a9b5c02352
Riya Vechiot <vechiotriya@gmail.com>
2026-07-04 15:50:09 +0530

    fix: proxy to ignore forget and reset password

--- apps/admin/src/proxy.ts
+++ apps/admin/src/proxy.ts
@@ -2,17 +2,20 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: ["/auth/:path*", "/menu-management", "/"],
+  matcher: ["/auth/login", "/menu-management", "/", "/categories"],
 };
+
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const token = request.cookies.get("token")?.value;
-
+  const token = request.cookies.get("token")?.value || false;
+  console.log("gaa",pathname,token);
+  
   if (!token && pathname !== "/auth/login") {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
   if (token && pathname === "/auth/login") {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
   return NextResponse.next();
 }
